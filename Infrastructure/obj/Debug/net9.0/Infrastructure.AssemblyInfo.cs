70ce06d9b6120e274ac34928b18a83018e43bf2c
SathiraWijeratne <sathira.w@expernetic.com>
2025-08-20 11:48:32 +0530

    Add Presentation webapi project with initial setup and configuration

--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b661c3289f62b5d57c2794ac88bb2df3edaa6257")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a95f95aea3e792faa9a8f2543bfbff25348d1bf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
