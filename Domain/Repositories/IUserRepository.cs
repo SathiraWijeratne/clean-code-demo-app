171c08fd245a485368de05b5e039a157b166ecea
SathiraWijeratne <sathira.w@expernetic.com>
2025-08-20 14:57:55 +0530

    Add IUserRepository interface for user data access operations

---
+++ Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using Domain.Entities;
+
+namespace Domain.Repositories;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> GetAllUsersAsync();
+    Task<User?> GetUserByIdAsync(int id);
+    Task AddUserAsync(User user);
+    Task UpdateUserAsync(User user);
+    Task DeleteUserAsync(int id);
+}
