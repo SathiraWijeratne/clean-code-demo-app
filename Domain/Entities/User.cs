4e8a2cd41c3b13151b9bfb7082f8ecf3ae5c1f20
SathiraWijeratne <sathira.w@expernetic.com>
2025-08-20 14:10:18 +0530

    Add User entity with validation attributes

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    [Required]
+    [StringLength(100, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 100 characters.")]
+    [RegularExpression(@"^[a-zA-Z]+(?:\\s[a-zA-Z]+)*$", ErrorMessage = "Name must contain only letters and spaces.")]
+    public required string Name { get; set; }
+}
