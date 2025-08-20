eeb4b9df277ad1615850dde9a6eb6117f0c39eac
SathiraWijeratne <sathira.w@expernetic.com>
2025-08-20 11:02:23 +0530

    Add Domain project reference to Application and update related files

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b9e9532f24095db4305b871eb064488d3ff8ab9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcd504984344f8c50d246427c53427be60df1314")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Presentation/obj/Debug/net9.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net9.0/Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcd504984344f8c50d246427c53427be60df1314")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+669ed527d4c0ce0ae060a1baae76fac2b74a6903")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
