dda43a8ead5c23310fd7cc7de767502c0f6dea26
SathiraWijeratne <sathira.w@expernetic.com>
2025-08-21 11:17:55 +0530

    Add missing implement MediatR interface in DeleteUserCommand and UpdateUserCommand classes

--- Application/Commands/DeleteUserCommand.cs
+++ Application/Commands/DeleteUserCommand.cs
@@ -1,12 +1,13 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using MediatR;
 
 namespace Application.Commands;
 
 /// <summary>
 /// Command for deleting an existing user.
 /// </summary>
-public class DeleteUserCommand
+public class DeleteUserCommand : IRequest
 {
     [Range(1, int.MaxValue, ErrorMessage = "Invalid user ID.")]
     public int Id { get; set; }
--- Application/Commands/UpdateUserCommand.cs
+++ Application/Commands/UpdateUserCommand.cs
@@ -1,13 +1,15 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using MediatR;
 
 namespace Application.Commands;
 
 /// <summary>
 /// Command for updating an existing user.
 /// </summary>
-public class UpdateUserCommand
+public class UpdateUserCommand : IRequest
 {
+    public int Id { get; set; }
     [Required]
     [StringLength(100, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 100 characters.")]
     [RegularExpression(@"^[a-zA-Z]+(?:\s[a-zA-Z]+)*$", ErrorMessage = "Name must contain only letters and spaces.")]
